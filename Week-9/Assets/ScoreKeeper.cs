9ba11c18bf5b85d940a93f646b419427fe17e369
husei <edin.huseinspahic@students.mq.edu.au>
2020-10-15 14:22:04 +1100

    Added UI Manager and Score Keeper

---
+++ Week-9/Assets/ScoreKeeper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public int pickUpValue;
+    UIManager uiManager;
+
+    private int score = 0;
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+    }
+
+    public void PickUpCoin()
+    {
+        score += pickUpValue;
+        uiManager.UpdateScoreText();
+    }
+}
