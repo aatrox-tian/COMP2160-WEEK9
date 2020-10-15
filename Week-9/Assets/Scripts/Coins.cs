e47b0ca82aa1bd3f06e0a601d4e7e866a9c1c2b4
aatroxgod <tianheng.zhao@students.mq.edu.au>
2020-10-15 11:24:59 +0800

    B's part done

---
+++ Week-9/Assets/Scripts/Coins.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	private void OnTriggerEnter2D(Collider2D collision)
+    {
+		  //if picked by player
+        if (collision.gameObject.tag == "Player")
+        {
+            //get score
+            score++;
+            //change score into text
+            text.text = score.ToString();
+            //destroy itself
+            Destroy();
+        }
+}
+}
---
+++ Week-9/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    
+    private float speed = 5;
+   
+    private Transform m_Transform;
+  
+    void Start()
+    {
+        
+        m_Transform = this.transform;
+        
+    }
+
+    void Update()
+    {
+        
+        playerMove();
+    }
+
+    void playerMove()
+    {	
+        
+            if (Input.GetKey(KeyCode.W))
+            {
+                m_Transform.localRotation = Quaternion.Euler(0, 0, 0);
+                m_Transform.Translate(Vector2.up * speed * Time.deltaTime);     
+            }
+          
+            if (Input.GetKey(KeyCode.S))
+            {
+                m_Transform.localRotation = Quaternion.Euler(0, 180, 0);
+                m_Transform.Translate(Vector2.down* speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                m_Transform.localRotation = Quaternion.Euler(0, 0, 0);
+                m_Transform.Translate(Vector2.left * speed * Time.deltaTime);
+}
+            if (Input.GetKey(KeyCode.D))
+            {
+                m_Transform.localRotation = Quaternion.Euler(0, 0, 0);
+                m_Transform.Translate(Vector2.right * speed * Time.deltaTime);
+            }
+            
+
+
+        
+        
+    }
+}
